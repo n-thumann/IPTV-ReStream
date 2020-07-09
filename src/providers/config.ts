c283aefaf733114f3a6de8e0efc3e68af8b4f1ac
nthumann <me@n-thumann.de>
2020-07-09 13:58:55 +0200

    Adds missing mcast_if log in config provider

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -17,7 +17,7 @@ class ConfigProvider {
         this.xspf_pathPrefix = process.env.XSPF_PATH_PREFIX || '';
         this.allow_unknown = (process.env.ALLOW_UNKNOWN === 'true' || process.env.ALLOW_UNKNOWN === '1') ? true : false;
         const logger = debug('iptv-restream:config');
-        logger(`Config loaded: HOST=${this.host}, PORT=${this.port}, XSPF_PROTOCOL=${this.xspf_protocol}, XSPF_HOST=${this.xspf_host}, XSPF_PATH_PREFIX=${this.xspf_pathPrefix}, ALLOW_UNKNOWN=${this.allow_unknown}`);
+        logger(`Config loaded: HOST=${this.host}, PORT=${this.port}, MCAST_IF=${this.mcast_if}, XSPF_PROTOCOL=${this.xspf_protocol}, XSPF_HOST=${this.xspf_host}, XSPF_PATH_PREFIX=${this.xspf_pathPrefix}, ALLOW_UNKNOWN=${this.allow_unknown}`);
     }
 }
 export default new ConfigProvider();
