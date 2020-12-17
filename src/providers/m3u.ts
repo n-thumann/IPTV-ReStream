5545b5a3b0059849504e568da855e361a3e192a3
Björn Stresing <bjoern.stresing@gmx.de>
2020-12-17 17:50:47 +0100

    Adds M3U playlist download (#87)

---
+++ src/providers/m3u.ts
@@ -0,0 +1,20 @@
+import stationProvider from './station';
+
+class M3UProvider {
+    public generateM3U(hostname: string) {
+
+        let m3uString = '#EXTM3U' + '\n';
+        let chNum = 1;
+
+        const stations = stationProvider.stations;
+        for (const station of stations) {
+            m3uString += '#EXTINF:-1';
+            m3uString += ` tvg-chno="${chNum}" tvg-name="${station.title}" tvg-logo="${station.image}" group-title="IPTV"`;
+            m3uString += ',' + station.title + '\n';
+            m3uString += `${hostname}/live/${station.mcast_source}@${station.mcast_group}:${station.mcast_port}` + '\n';
+            chNum++
+        }
+        return m3uString;
+    }
+}
+export default new M3UProvider();
