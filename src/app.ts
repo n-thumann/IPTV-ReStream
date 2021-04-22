f1d6b1d98e48d64847ed17cdabc1ebb4fd485d16
nthumann <me@n-thumann.de>
2021-04-22 19:03:01 +0200

    Removes Node v13.1.0 version check

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import * as path from 'path';
 import morgan from 'morgan';
 import http from 'http';
-import semver from 'semver';
 import debug from 'debug';
 import indexRouter from './routes/index';
 import statusRouter from './routes/status';
@@ -10,11 +9,6 @@ import liveRouter from './routes/live';
 import stationRouter from './routes/station';
 import configProvider from './providers/config';
 import { SignalConstants } from 'node:os';
-
-if (semver.lt(process.versions.node, '13.1.0')) {
-    console.error('NodeJS v13.0.2 (or higher) is required!');
-    process.exit(1);
-}
 
 const app = express();
 const logger = debug('iptv-restream:server');
